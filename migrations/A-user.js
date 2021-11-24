6c5c89b9eaad65ee4a0cfa9abfd552c6b4dc3df2
atulbinmile <atul@binmile.com>
2021-11-24 14:24:52 +0530

    user migration file

---
+++ migrations/A-user.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('user', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    },
+    {
+      underscored:true,
+      freezeTableName:true
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('user');
+  }
+};
