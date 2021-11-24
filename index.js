a9c805a916691b11725e30f8805878abecae2fa7
atulbinmile <atul@binmile.com>
2021-11-24 14:23:16 +0530

    index file

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const UserController = require("./controllers/userController");
+const express = require("express");
+const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+//routes
+app.post("/create", UserController.create);
+
+app.get("/find/:id", UserController.find);
+
+app.put("/update/:id", UserController.update);
+
+app.delete("/delete/:id", UserController.delete);
+
+//server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
